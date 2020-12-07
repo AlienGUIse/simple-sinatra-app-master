f9b4d26412c3c93d3f3642b0996cf814985c7397
Wei Wang <wei.wang@sortedservices.com>
2020-12-07 14:01:28 +1100

    Removed policy

--- deploy/ecr/ecr.tf
+++ deploy/ecr/ecr.tf
@@ -10,12 +10,3 @@ resource "aws_ecr_lifecycle_policy" "ecr_delete_old_images" {
   repository = aws_ecr_repository.simple-sinatra.name
   policy     = data.template_file.lifecycle_json.rendered
 }
-
-data "template_file" "permissions_json" {
-  template = file("permissions.json")
-}
-
-resource "aws_ecr_repository_policy" "allow_access" {
-  repository = aws_ecr_repository.simple-sinatra.name
-  policy     = data.template_file.permissions_json.rendered
-}
